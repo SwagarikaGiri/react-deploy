98afc6f18c8fe0c1413656788f5086aa55a2e1ee
SwagarikaGiri <swagaarika95@gmail.com>
2020-08-04 13:14:50 +0530

    Updates

---
+++ precache-manifest.53e5a37295e562bd6c87bab0dd9ac801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f56a2d82c7a268b159227e6b7b40e99",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "cacb6591dc3d52ce0912",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a10a78df6c76ca3a627",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b1b31c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cacb6591dc3d52ce0912",
+    "url": "/react-deploy/static/js/main.e0d6bfb2.chunk.js"
+  },
+  {
+    "revision": "9a6b4006b58ad85b50f6",
+    "url": "/react-deploy/static/js/runtime-main.3e11c617.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
