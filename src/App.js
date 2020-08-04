ff86f70acf7f78db5dbce2c307d812cf0652a340
SwagarikaGiri <swagaarika95@gmail.com>
2020-08-04 13:13:05 +0530

    deploy my react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <p>He there Its swagarika lets check git deploymment</p>
+     
+    </div>
+  );
+}
+
+export default App;
